3692303366a118d43af6de05355a41b71137f255
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-02-02 18:34:59 +0100

    feat(models): add Restaurant model with relationships and fillable fields

---
+++ app/Models/Restaurant.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Restaurant extends Model
+{
+    use HasFactory;
+    protected $table = 'Restaurants';
+    protected $fillable = [
+        'nom',
+        'localisation',
+        'capacite',
+    ];
+
+    public function TypeCuisine()
+    {
+        return $this->belongsTo(TypeCuisine::class, 'typeCuisine_id');
+    }
+
+    public function Photo()
+    {
+        return $this->hasMany(Photo::class);
+    }
+
+    public function Horaire()
+    {
+        return $this->hasMany(Horaire::class);
+    }
+
+    public function Menu()
+    {
+        return $this->hasMany(Menu::class);
+    }
+
+    public function User()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function Favoris()
+    {
+        return $this->hasMany(Favoris::class);
+    }
+    
+}

2e13fd2f414041d7219c14bc837244554d9e204c
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-02-04 15:33:37 +0100

    feat(seeders): update RolePermissionSeeder to add new permissions and roles

--- database/seeders/RolePermissionSeeder.php
+++ database/seeders/RolePermissionSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class RolePermissionSeeder extends Seeder
 {
@@ -12,6 +14,50 @@ class RolePermissionSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        $permissions = [
+            'view restaurants',
+            'create restaurants',
+            'edit restaurants',
+            'delete restaurants',
+            'view menus',
+            'create menus',
+            'edit menus',
+            'delete menus',
+            'view plats',
+            'create plats',
+            'edit plats',
+            'delete plats',
+            'manage users',
+            'manage roles',
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::create(['name' => $permission]);
+        }
+
+        $adminRole = Role::create(['name' => 'admin']);
+        $adminRole->givePermissionTo(Permission::all());
+
+        $ownerRole = Role::create(['name' => 'restaurant_owner']);
+        $ownerRole->givePermissionTo([
+            'view restaurants',
+            'create restaurants',
+            'edit restaurants',
+            'view menus',
+            'create menus',
+            'edit menus',
+            'view plats',
+            'create plats',
+            'edit plats',
+        ]);
+
+        $userRole = Role::create(['name' => 'user']);
+        $userRole->givePermissionTo([
+            'view restaurants',
+            'view menus',
+            'view plats',
+        ]);
     }
 }

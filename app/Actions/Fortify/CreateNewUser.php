6f9418383f86ed28f3e63de612fe7bbd547a7965
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-02-05 12:59:12 +0100

    feat(Fortify): simplify role assignment logic in user creation

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -33,7 +33,7 @@ class CreateNewUser implements CreatesNewUsers
             'password' => Hash::make($input['password']),
         ]);
 
-        if (isset($input['role']) && $input['role'] !== 'admin') {
+        if (isset($input['role'])) {
             $user->assignRole($input['role']);
         }
 

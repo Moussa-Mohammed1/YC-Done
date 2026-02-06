60aea548f5936d1a602852d44f6701cd0eaf621a
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-02-06 19:17:22 +0100

    fix(home): update index method to handle restaurant query and horaires filtering

--- app/Http/Controllers/User/HomeController.php
+++ app/Http/Controllers/User/HomeController.php
@@ -8,13 +8,29 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+
         $userRestaurant = [];
         if (auth()->user()->hasRole('restaurant_owner')) {
-            $userRestaurant = Restaurant::with('photos', 'types_cuisine', 'menus', 'horaires')->where('user_id', '=', auth()->user()->id)->get();
+            $userRestaurant = Restaurant::with('photos', 'typeCuisine', 'menus', 'horaires')->where('user_id', '=', auth()->user()->id)->get();
         }
-        $restaurants = Restaurant::with('photos', 'typeCuisine', 'menus', 'horaires')->paginate(8);
+        if ($request->filled('horaire') || $request->filled('query')) {
+            $heureOuverture = '11:00';
+            $heureFermeture = '23:00';
+            if ($request->filled('horaire')) {
+                $heureOuverture = explode('-', $request->input('horaire'))[0];
+                $heureFermeture = explode('-', $request->input('horaire'))[1];
+            }
+            $query = $request->input('query');
+            $restaurants = Restaurant::query();
+        }
+
+        $restaurants = Restaurant::with('photos', 'typeCuisine', 'menus', 'horaires')
+                        ->withExists(['favoris as isFavoris' => function ($q){
+                             $q->where('user_id', auth()->id());
+                        }])
+                        ->paginate(8);
         return view('user.home', compact('restaurants', 'userRestaurant'));
     }
 }

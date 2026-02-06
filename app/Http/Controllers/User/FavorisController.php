ef229f0cb7dd0c3f7634038f3f2c9e2d8dd56cff
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-02-06 19:16:51 +0100

    feat(favoris): add FavorisController for managing user favorites store and destroy

---
+++ app/Http/Controllers/User/FavorisController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use App\Models\Favoris;
+use Illuminate\Http\Request;
+use Symfony\Component\Console\Input\Input;
+
+class FavorisController extends Controller
+{
+    public function destroy(Request $request)
+    {
+        $user_id = $request->input('user_id');
+        $restaurant_id = $request->input('restaurant_id');
+        Favoris::where('user_id', '=', $user_id)
+                ->where('restaurant_id', '=', $restaurant_id)
+                ->delete();
+        return response()->json([
+            'message' => 'Supprimé de favoris aved success'
+        ]);
+    }
+    public function store(Request $request)
+    {
+        $user_id = $request->input('user_id');
+        $restaurant_id = $request->input('restaurant_id');
+        Favoris::create([
+            'user_id' => $user_id,
+            'restaurant_id' => $restaurant_id
+        ]);
+        return response()->json([
+            'message' => 'Ajouté aux favoris avec success'
+        ]);
+    }
+}

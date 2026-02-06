9057871f268187dd6588a5e47ac0b1e14bb4be82
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-02-06 21:17:17 +0100

    feat(favoris): add routes for managing favorites and restaurant creation and retrieving

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,10 @@
 
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\RestaurantController;
+use App\Http\Controllers\User\RestaurantController as r;
 use App\Http\Controllers\Admin\PermissionController;
 use App\Http\Controllers\User\HomeController;
+use App\Http\Controllers\User\FavorisController;
 use App\Http\Controllers\UserController;
 use GuzzleHttp\Middleware;
 use Illuminate\Support\Facades\Route;
@@ -38,3 +40,11 @@ Route::post('/admin/assign-role', [UserController::class, 'assignRole'])
         ->middleware('auth', 'role:admin')
         ->name('admin.assign-role');
 Route::get('/home', [HomeController::class, 'index'])->middleware('auth')->name('user.home');
+Route::delete('/favoris/delete', [FavorisController::class, 'destroy'])
+        ->middleware('auth');
+Route::post('/favoris/add',[FavorisController::class, 'store'])->middleware('auth');
+
+Route::get('/Restaurants/create',   [r::class, 'create'])->middleware('auth')->name('create.restaurant');
+Route::get('/favoris', [FavorisController::class, 'index'])->middleware('auth')->name('user.favoris');
+Route::get('/restaurant/{restaurant}', [r::class, 'show'])->middleware('auth')->name('show.restaurant');
+Route::get('/mesRestaurants', [r::class, 'owner'])->middleware('auth', 'role:restaurant_owner')->name('myrestaurants');

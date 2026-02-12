0cac5a698edc6acc9c8fd21d5d704069bf47c450
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-02-12 11:48:14 +0100

    feat(migration): fix reservation table name and add closed column to horaires table

--- database/migrations/2026_02_11_233651_create_reservations_table.php
+++ database/migrations/2026_02_11_233651_create_reservations_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('reservation', function (Blueprint $table) {
+        Schema::create('reservations', function (Blueprint $table) {
             $table->id();
             $table->dateTime('day');
             $table->foreignId('user_id')->constrained('users');
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('reservation');
+        Schema::dropIfExists('reservations');
     }
 };

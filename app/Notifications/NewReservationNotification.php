9c050fae0194a1505b4fe1aa980cf18ce4d84107
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-02-14 04:53:54 +0100

    feat(notification): add NewReservationNotification for database notifications on new reservations

---
+++ app/Notifications/NewReservationNotification.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Reservation;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class NewReservationNotification extends Notification
+{
+    use Queueable;
+
+    public $reservation;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(Reservation $reservation)
+    {
+        $this->reservation = $reservation;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['database'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->line('The introduction to the notification.')
+            ->action('Notification Action', url('/'))
+            ->line('Thank you for using our application!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            'reservation_id' => $this->reservation->id,
+            'client_name' => $this->reservation->user->name,
+            'restaurant_nom' => $this->reservation->restaurant->nom,
+            'guests' => $this->reservation->personnes,
+            'date' => $this->reservation->day,
+            'time' => $this->reservation->startHour,
+            'message' => 'New reservation from ' . $this->reservation->user->name,
+        ];
+    }
+}
+    

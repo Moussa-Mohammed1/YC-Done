43d419e4a4ae8b1f712b9a897ead1a11a47b3397
Moussa-Mohammed1 <moussamohammed2005@gmail.com>
2026-02-14 04:56:24 +0100

    feat(routes): add reservation and payment routes with availability checks

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,14 @@
 <?php
-
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\RestaurantController;
 use App\Http\Controllers\User\RestaurantController as r;
 use App\Http\Controllers\Admin\PermissionController;
 use App\Http\Controllers\User\HomeController;
+use App\Http\Controllers\User\ReservationController;
 use App\Http\Controllers\User\FavorisController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\PaymentController;
+use App\Services\ReservationService;
 use GuzzleHttp\Middleware;
 use Illuminate\Support\Facades\Route;
 
@@ -23,6 +25,7 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+Route::get('/{restaurant}/reservations', [ReservationController::class, 'show'])->name('reservations');
 Route::get('/dashboard' , [DashboardController::class, 'index'])
         ->middleware('auth','role:admin')
         ->name('admin.dashboard');
@@ -44,9 +47,28 @@ Route::delete('/favoris/delete', [FavorisController::class, 'destroy'])
         ->middleware('auth');
 Route::post('/favoris/add',[FavorisController::class, 'store'])->middleware('auth');
 
-Route::get('/Restaurants/create',   [r::class, 'create'])->middleware('auth')->name('create.restaurant');
-Route::post('/Restaurants/store',   [r::class, 'store'])->middleware('auth')->name('store.restaurant');
-Route::get('/favoris', [FavorisController::class, 'index'])->middleware('auth')->name('user.favoris');
-Route::get('/restaurant/{restaurant}', [r::class, 'show'])->middleware('auth')->name('show.restaurant');
-Route::get('/restaurant/{restaurant}/edit', [r::class, 'edit'])->middleware('auth')->name('edit.restaurant');
-Route::get('/mesRestaurants', [r::class, 'owner'])->middleware('auth', 'role:restaurant_owner')->name('myrestaurants');
+Route::get('/Restaurants/create',   [r::class, 'create'])
+        ->middleware('auth')->name('create.restaurant');
+Route::post('/Restaurants/store',   [r::class, 'store'])
+        ->middleware('auth')->name('store.restaurant');
+Route::get('/favoris', [FavorisController::class, 'index'])
+        ->middleware('auth')->name('user.favoris');
+Route::get('/restaurant/{restaurant}', [r::class, 'show'])
+        ->middleware('auth')->name('show.restaurant');
+Route::get('/restaurant/{restaurant}/edit', [r::class, 'edit'])
+        ->middleware('auth')->name('edit.restaurant');
+Route::get('/mesRestaurants', [r::class, 'owner'])
+        ->middleware('auth', 'role:restaurant_owner')->name('myrestaurants');
+
+Route::get('/restaurant/{restaurant}/slots', 'App\Services\ReservationService@getDayAvailableHours')
+        ->middleware('auth')
+        ->name('restaurants.slots');
+
+Route::get('/restaurant/{restaurant}/check-availability', 'App\Services\ReservationService@checkAvailability')
+        ->middleware('auth')
+        ->name('restaurants.checkAvailability');
+
+Route::post('/payment', [PaymentController::class, 'pay'])
+        ->middleware('auth')
+        ->name('payment');
+
